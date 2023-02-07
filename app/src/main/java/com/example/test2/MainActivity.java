50605d64b84dd3a9573ee8f12bc87416a662d4f9
Pulikovskyy <117446427+Pulikovskyy@users.noreply.github.com>
2023-02-07 15:09:42 +0800

    Add files via upload
    - Added Firebase for login and register fields.
    - Removed TEST2 banner.

--- app/src/main/java/com/example/test2/MainActivity.java
+++ app/src/main/java/com/example/test2/MainActivity.java
@@ -1,12 +1,25 @@
 package com.example.test2;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //button for register here
         TextView btn = findViewById(R.id.registerHere);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //button for forgetpassword
         TextView btn2 = findViewById(R.id.forgotPass);
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,13 +46,64 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //button for login
+        EditText EDemail = findViewById(R.id.editTextTextEmailAddress);
+        EditText EDpass = findViewById(R.id.editTextTextPassword);
         Button btn3 = findViewById(R.id.button_main);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        /*
+REMOVE COMMENT TO ALLOW LOGIN TO PROCEED WITHOUT CREDENTIALS
+
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this,MainMenu.class));
+
             }
         });
+ */
+        /*
+        COMMENT ENTIRE BLOCK  IF YOU NEED TO DEBUG WITHOUT LOGGING IN!
+         */
+        //START HERE
+        //btn for login logic
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email,password;
+
+                email = String.valueOf(EDemail.getText());
+                password = String.valueOf(EDpass.getText());
+                if(TextUtils.isEmpty(email)) {
+                    Toast.makeText(MainActivity.this, "Email cannot be empty!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(password)) {
+                    Toast.makeText(MainActivity.this, "Password cannot be empty!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this, "Login Successful!",
+                                            Toast.LENGTH_SHORT).show();
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    startActivity(new Intent(MainActivity.this,MainMenu.class));
+
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                } else {
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(MainActivity.this, "Login failed. Re-enter credentials",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+                //END HERE
     }
 /*
     public void homelayoutButton(View view){
